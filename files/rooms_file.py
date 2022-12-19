62e73b58f5e942106b6455b1fcf564e1cad836ec
Kristian Tijben <c.w.tijben@student.utwente.nl>
2022-12-20 00:34:18 +0100

    Started Loading

---
+++ files/rooms_file.py
@@ -0,0 +1,14 @@
+# This room entry is not used and is intended as a sample template for adding more rooms.
+room_name = {
+    "name": "Sample Name",
+    "side": "a/b",
+    "theme": "Sample Theme",
+    "coordinates": [[0, 0, 0]],
+    "links": [[-1, 0, 1, 6, "entry/exit"]],
+    "hexes": 10
+}
+
+
+available_rooms = [
+    # room names
+]
--- load.py
+++ load.py
@@ -1,24 +1,49 @@
 from dungeon import Dungeon
+from room import Room
+from monster import Monster
 
+import files.chests_file, files.dungeons_file, files.monsters_file, files.rooms_file, files.rules_file
+
+def load_rules():
+    all_rules = files.rules_file.rules
+    return all_rules
+
+
+def load_chests():
+    all_chests = files.chests_file.chests
+    return all_chests
+
+
+def load_rooms():
+
+
+def load_monsters():
+    all_monsters = []
+    for monster_type in files.monsters_file.available_monsters:
+        monster_name = monster_type["name"]
+        monster_max = monster_type["max"]
+        monster_difficulty = monster_type["difficulty"]
+        monster_theme = monster_type["theme"]
+        all_monsters.append(Monster(monster_name, monster_max, monster_difficulty, monster_theme))
+    return all_monsters
 
 def load_dungeon(dungeon_name):
     dungeon_data =
 
-    goal_data = dungeon_data.iat[0, 0]
-    rules_data = dungeon_data.iat[1, 0]
-    rooms_data = dungeon_data.iat[2, 0]
-    connections_data = dungeon_data.iat[3, 0]
-    monster_data = dungeon_data.iat[4, 0]
-    obstacles_data = dungeon_data.iat[5, 0]
-    traps_data = dungeon_data.iat[6, 0]
-    h_terrain_data = dungeon_data.iat[7, 0]
-    d_terrain_data = dungeon_data.iat[8, 0]
-    chest_data = dungeon_data.iat[9, 0]
-    coins_data = dungeon_data.iat[10, 0]
-    theme_data = dungeon_data.iat[11, 0]
-    placements_data = dungeon_data.iat[12, 0]
-    start_data = dungeon_data.iat[13, 0]
-
+    goal_data = dungeon_data[0]
+    rules_data = dungeon_data[1]
+    rooms_data = dungeon_data[2]
+    connections_data = dungeon_data[3]
+    monster_data = dungeon_data[4]
+    obstacles_data = dungeon_data[5]
+    traps_data = dungeon_data[6]
+    h_terrain_data = dungeon_data[7]
+    d_terrain_data = dungeon_data[8]
+    chest_data = dungeon_data[9]
+    coins_data = dungeon_data[10]
+    theme_data = dungeon_data[11]
+    placements_data = dungeon_data[12]
+    start_data = dungeon_data[13]
 
     new_dungeon = Dungeon(goal_data, rules_data, rooms_data, connections_data, monster_data, obstacles_data, traps_data, h_terrain_data, d_terrain_data, chest_data, coins_data, theme_data, placements_data, start_data)
     return new_dungeon
